68e0f5935f16f804e646e86559dde4db2f9ca366
hajdukd <daniel.piotr.hajduk@gmail.com>
2019-08-24 04:28:56 -0400

    fix: run in background

--- busybox_examples/run_simple_http_server_local_command.sh
+++ busybox_examples/run_simple_http_server_local_command.sh
@@ -1,1 +1,2 @@
-nohup while true; do { echo -e 'HTTP/1.1 200 OK\r\n'; echo 'hello world'; } | nc -l -p  8080; done > /tmp/simplehttpserver.log 2>&1
+#!/bin/bash
+nohup bash -c  "while true; do { echo -e 'HTTP/1.1 200 OK\\r\\n'; echo 'hello world'; } | nc -l -p  8080; done" > /tmp/simplehttpserver.log 2>&1 &
